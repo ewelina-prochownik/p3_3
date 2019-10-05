1c73eda8886f7a0fc0b0f40082ba72ba69e7431e
ewelina-prochownik <ewe.prochownik@gmail.com>
2019-10-05 14:42:39 +0200

    Start

---
+++ Lab_3.1/Funkcje.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_3._1
+{
+    public class Funkcje
+    {
+        public float ObliczSrednia(float suma, int iloscElementow)
+        {
+            return (float) suma / iloscElementow;
+        }
+    }
+}
--- Lab_3.1/Program.cs
+++ Lab_3.1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*Zadanie ze srednia ucznia - napisac program, ktory pomaga policzyc srednia dla danego ucznia, jeden przedmiot, nie wiemy ile
+bedzie ocen. Średnia arytmetyczna*/
+
+using System;
+using System.Text;
 
 namespace Lab_3._1
 {
@@ -6,7 +10,39 @@ namespace Lab_3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Funkcje obiekt = new Funkcje();
+            
+            StringBuilder builder = new StringBuilder();
+
+            string text = "@";
+            float sum = 0;
+            float result = 0;
+
+            while (text != string.Empty)
+            {
+                text = Console.ReadLine();
+                builder.Append(text + ";");
+            }
+
+            string gradesString = builder.ToString();
+            string[] grades = gradesString.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            float[] gradesInt = new float[grades.Length];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                gradesInt[i] = Convert.ToSingle(grades[i]);
+            }
+
+            foreach (float grade in gradesInt)
+            {
+                sum += grade;
+            }
+
+            result = obiekt.ObliczSrednia(sum, gradesInt.Length);
+            double result2 = System.Math.Round(result, 2);
+            Console.WriteLine(result2);
         }
     }
 }
